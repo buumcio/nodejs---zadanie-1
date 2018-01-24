dcfca7fd507446cd3402ca888fa658f9d523b0c3
Magdalena Szymczak <mwozniak.kontakt@gmail.com>
2018-01-24 22:21:32 +0100

    Add file program.js

---
+++ program.js
@@ -0,0 +1,25 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+ 		switch (instruction) {
+ 			case ('/exit'):
+    			process.stdout.write('Quitting app!\n');
+        		process.exit();
+        		break;
+    		case ('/process.env'):
+    			console.log(process.env);
+    			break;
+    		default:
+    			//process.stdeer.write('Wrong instruction!\n'); - tutaj wyrzuca błąd, nie rozpoznaje słowa write
+    			process.stdout.write('Wrong instruction!\n');
+		}
+	}
+});
+
+
+
+
+ 
